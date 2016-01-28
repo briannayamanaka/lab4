24f5b88eff1ad411699e69efb918dd4019a616fa
briannayamanaka <briannayamanaka@gmail.com>
2016-01-28 20:09:04 +0000

    first commit

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -31,14 +33,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+		console.log('Express server listening on port ' + app.get('port'));
+		});
